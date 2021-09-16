179f5737b7a3fd07383d3a066e2b569c1d9f1488
wukui <3128006406@qq.com>
2021-09-16 16:59:47 +0800

    fix: "时间线数据渲染"

--- src/views/profile/components/Timeline.vue
+++ src/views/profile/components/Timeline.vue
@@ -15,29 +15,34 @@
 export default {
   data() {
     return {
-      timeline: [
-        {
-          timestamp: '2019/4/20',
-          title: 'Update Github template',
-          content: 'PanJiaChen committed 2019/4/20 20:46'
-        },
-        {
-          timestamp: '2019/4/21',
-          title: 'Update Github template',
-          content: 'PanJiaChen committed 2019/4/21 20:46'
-        },
-        {
-          timestamp: '2019/4/22',
-          title: 'Build Template',
-          content: 'PanJiaChen committed 2019/4/22 20:46'
-        },
-        {
-          timestamp: '2019/4/23',
-          title: 'Release New Version',
-          content: 'PanJiaChen committed 2019/4/23 20:46'
-        }
-      ]
+      timeline: []
     }
+  },
+  mounted() {
+    const data = [{
+      timestamp: '2019/4/20',
+      title: 'Update Github template',
+      content: 'PanJiaChen committed 2019/4/20 20:46'
+    },
+    {
+      timestamp: '2019/4/21',
+      title: 'Update Github template',
+      content: 'PanJiaChen committed 2019/4/21 20:46'
+    },
+    {
+      timestamp: '2019/4/22',
+      title: 'Build Template',
+      content: 'PanJiaChen committed 2019/4/22 20:46'
+    },
+    {
+      timestamp: '2019/4/23',
+      title: 'Release New Version',
+      content: 'PanJiaChen committed 2019/4/23 20:46'
+    }]
+    // 获取时间线数据
+    data.forEach((row) => {
+      this.timeline.push(row)
+    })
   }
 }
 </script>
